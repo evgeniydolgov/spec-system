7dffc3e44cb629cab430e76c655d036dfffb8074
Evgeniy Dolgov <amyr214519@gmail.com>
2023-04-09 17:06:44 +0300

    done work with mask

--- src/components/Registration/Registration.tsx
+++ src/components/Registration/Registration.tsx
@@ -1,36 +1,70 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import styles from './Registration.module.css';
 import logo from '../../assets/png/logo.png';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import InputMask from 'react-input-mask';
+
+type Inputs = {
+  telephone: string;
+  password: string;
+};
 
 export const Registration = () => {
-  const [tel, setTel] = useState<string | undefined>();
+  const [tel, setTel] = useState<string>(' ');
 
-  const setTelephoneNumber = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTel(event.target.value.replace(/1-9/g, ''));
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>({ mode: 'onSubmit' });
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    return console.log(data);
   };
-  useEffect(() => {
-    console.log(tel);
-  }, [tel]);
 
   return (
     <div className={styles.reg_container}>
       <div>
         <img src={logo} alt='firm logotype' />
       </div>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label>
           Введите логин
           <div>
-            <input type='tel' onChange={(e) => setTelephoneNumber(e)} value={tel} />
+            <InputMask
+              type='tel'
+              mask='+7 999 999 99 99'
+              maskPlaceholder={''}
+              placeholder={'+7 999 999 99 99'}
+              {...register('telephone', {
+                required: true,
+                minLength: {
+                  value: 16,
+                  message: 'Введите ваш логин',
+                },
+              })}
+            />
           </div>
         </label>
+        {errors.telephone && <p>{errors.telephone?.message}</p>}
         <label>
           Введите пароль
           <div>
-            <input type='password' />
+            <input
+              type='password'
+              {...register('password', {
+                required: true,
+                minLength: {
+                  value: 6,
+                  message: 'Введите ваш пароль',
+                },
+              })}
+            />
           </div>
         </label>
+        {errors.password && <p>{errors.password?.message}</p>}
         <p>Забыли пароль?</p>
+        <input type='submit' name='Войти' />
       </form>
     </div>
   );
